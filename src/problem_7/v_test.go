6a30f1f0a676c77c0e1ffeb0d6b2d11142aa315b
tmomy <datmomy@gmail.com>
2018-09-27 23:09:32 +0800

    add reverse func

--- src/problem_7/v_test.go
+++ src/problem_7/v_test.go
@@ -1,2 +1,25 @@
 //@author: lizhijun
 package problem_7
+
+import (
+	"testing"
+)
+
+func TestReverse(t *testing.T)  {
+	for _, unit := range []struct {
+		s        int
+		expected int
+	}{
+		{1534236469, 0},
+		{-123, -321},
+		{-320, -23},
+		{1534236469, 0},
+	}{
+
+		if actually:= reverse(unit.s); actually!=unit.expected {
+			t.Errorf("longestPalindrome2:[%v],actually:[%v]",unit, actually)
+		}
+
+	}
+}
+
